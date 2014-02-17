654888d25221e4ff98e4727f871a740ee8b64551
Ellie Frost <web@stillinbeta.com>
2014-02-17 12:56:32 -0800

    Try to database

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
+import logging
 import os
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
-from tornado.options import define, options
+from models import create_tables
+
+from tornado.options import define
 define("port", default=5000, help="run on the given port", type=int)
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.set_header('content-type', 'text/plain')
         self.write("Hello, world")
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+
+class MyApplication(tornado.web.Application):
+    def __init__(self, db_url, **settings):
+        engine = create_engine(db_url, echo=True)
+        create_tables(engine)
+
+        self.db = scoped_session(sessionmaker(bind=engine))
+
+        handlers = [
+            (r"/", MainHandler),
+        ]
+
+        super().__init__(handlers, **settings)
+
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    http_server = tornado.httpserver.HTTPServer(application)
+
+    try:
+        db_url = os.environ["DATABASE_URL"]
+    except KeyError:
+        logging.error("Improperly configured database!")
+        raise SystemExit(1)
+
+    app = MyApplication(db_url)
+
+    http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(os.environ.get("PORT", 5000))
 
-# start it up
+    # start it up
     tornado.ioloop.IOLoop.instance().start()
-
---
+++ models.py
@@ -0,0 +1,17 @@
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
+
+class Repository():
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    owner = Column(String)
+    repo = Column(String)
+    board = Column(String)
+    new_list = Column(String)
+
+
