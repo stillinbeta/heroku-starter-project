0cbf6e509b3a8f0857b01f7df13bc093026d3a39
Ellie Frost <web@stillinbeta.com>
2014-02-19 11:02:20 -0800

    Web hooks return urlencoded POST data, not JSON

--- github_handlers.py
+++ github_handlers.py
@@ -1,6 +1,7 @@
 import json
 from pprint import pformat
 import logging
+import urllib.parse
 
 from tornado.web import RequestHandler, HTTPError, URLSpec
 import tornado.escape
@@ -11,15 +12,17 @@ class WebHookEndpoint(RequestHandler, RepositoryMixin):
     def post(self, repo_id, event):
         _ = self.get_or_400(repo_id)
 
+        payload = self.get_body_argument('payload', default=None)
+        unquoted = urllib.parse.unquote(payload)
         try:
-            decoded = json.loads(tornado.escape.to_unicode(self.request.body))
+            decoded = json.loads(tornado.escape.to_unicode(unquoted))
         except ValueError:
             raise HTTPError(
                 400,
                 "Invalid body received {}".format(self.request.body)
             )
 
-        logging.info(pformat(decoded))
+        print(unquoted)
 
         self.set_status(204)
         self.finish()
