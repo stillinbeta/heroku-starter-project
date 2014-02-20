04f0366ab472ce9fdd6ee7cd2fcb09fd6975f982
Ellie Frost <web@stillinbeta.com>
2014-02-20 11:39:47 -0800

    First attempt at handling mentions

--- github_handlers.py
+++ github_handlers.py
@@ -1,5 +1,7 @@
 import json
 import logging
+import re
+import unittest
 import urllib.parse
 
 from tornado.web import RequestHandler, HTTPError, URLSpec
@@ -47,7 +49,7 @@ class WebHookEndpoint(RequestHandler, RepositoryMixin):
 
         if user is None:
             members = None
-            if add_member is not None:
+            if add_member:
                 logging.warning(
                     "No Trello user found for github user {}".format(
                         github_user
@@ -74,6 +76,51 @@ class WebHookEndpoint(RequestHandler, RepositoryMixin):
                 card_id,
             )
         )
+
+    @gen.coroutine
+    def _add_members_from_comments(self, repo, issue):
+        body = issue['comment']['body']
+        issue_number = issue['issue']['number']
+
+        mentions = find_mentions(body)
+        if not mentions:
+            logging.info(
+                "Comment '{}' has no mentions, ignoring.".format(body)
+            )
+            return
+
+        db = self.application.db
+
+        card = db.query(Issue)\
+            .filter(Issue.issue_id == issue_number)\
+            .filter(Issue.repo_id == repo.id).first()
+        if card is None:
+            logging.error("I don't have a card for {}/{} issue #{}".format(
+                repo.owner,
+                repo.repo,
+                issue_number
+            ))
+            return
+
+        users = self.application.db.query(User)\
+            .filter(User.github_user.in_(mentions)).first()
+        for user in users:
+            # TODO make completely async
+            yield self.trello.add_member_to_card(card.card_id,
+                                                 user.trello_user)
+            mentions.remove(user.github_user)
+
+        if mentions:
+            logging.warning(
+                "Couldn't find trello user for github users {}".format(
+                    ", ".join(mentions)
+                )
+            )
+
+        logging.info("Added {} users to card {}".format(
+            len(users),
+            card.card_id
+        ))
 
     @gen.coroutine
     def post(self, repo_id, event):
@@ -109,8 +156,48 @@ class WebHookEndpoint(RequestHandler, RepositoryMixin):
         elif (event == 'pull_request_review_comment' or
               event == 'issue_comment'):
             logging.info('Comment!')
+            if 'issue' not in decoded:
+                # TODO: pull_request_review_comment events don't have issue
+                logging.warning("Issue not found for comment event, ignoring")
+            else:
+                yield self._add_members_from_comments(repo, decoded)
+
         else:
             logging.warning("Hook receieved unknown event {}".format(event))
 
 
 handlers = [URLSpec(r'/hooks/(\d+)/(\w+)', WebHookEndpoint, name='hook')]
+
+
+def find_mentions(comment):
+    """
+    As best I can tell there are no restricions what charachters are valid
+    in github usernames.
+    See the test cases below for what this expression should match
+    """
+    return re.findall('(?:\s|^)@(\w+)', comment)
+
+
+class FindMentionsTests(unittest.TestCase):
+    def test_find_multiple(self):
+        comment = "Hey @lizkf @stillinbeta could you take a look at this?"
+        self.assertEqual(find_mentions(comment), ['lizkf', 'stillinbeta'])
+
+    def test_find_start_of_word(self):
+        comment = "@lizkf can you pick this up?"
+        self.assertEqual(find_mentions(comment), ['lizkf'])
+
+    def test_ignore_emails(self):
+        comment = "Hey @lizkf email steve@infocorp.net"
+        self.assertEqual(find_mentions(comment), ['lizkf'])
+
+    def test_handle_unicode_usernames(self):
+        comment = "Hey @zoë can you take a look at this"
+        self.assertEqual(find_mentions(comment), ['zoë'])
+
+    def test_no_matches(self):
+        comment = "This is good, merged"
+        self.assertEqual(find_mentions(comment), [])
+
+if __name__ == "__main__":
+    unittest.main()
